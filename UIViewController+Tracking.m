fe096f76d98a674ece1a55d39a29017073412bcb
@tPlisk <ryanpliske@yahoo.com>
2015-07-18 10:36:27 -0500

    Initial Commit

---
+++ UIViewController+Tracking.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Tracking.m
+//  interviewPrepWithObjC
+//
+//  Created by Ryan Pliske on 7/18/15.
+//  Copyright (c) 2015 Ryan Pliske. All rights reserved.
+//
+
+#import "UIViewController+Tracking.h"
+
+@implementation UIViewController (Tracking)
+
+@end
---
+++ interviewPrepWithObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  interviewPrepWithObjC
+//
+//  Created by Ryan Pliske on 7/17/15.
+//  Copyright (c) 2015 Ryan Pliske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
