12254085364817c3cf5ea77d9af6f829fa5edfcf
@tPlisk <ryanpliske@yahoo.com>
2015-07-18 11:28:54 -0500

    added UIViewController Tracking Category for Method Swizzling

--- interviewPrepWithObjC/UIViewController+Tracking.m
+++ interviewPrepWithObjC/UIViewController+Tracking.m
@@ -6,8 +6,21 @@
 //  Copyright (c) 2015 Ryan Pliske. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import "UIViewController+Tracking.h"
 
 @implementation UIViewController (Tracking)
+
+
++ (void)load {
+    
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+    });
+    
+    
+}
 
 @end
