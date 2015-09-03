4f1a42aa03f570460f939d0bb7516dc0d0bc8328
@tPlisk <ryanpliske@yahoo.com>
2015-09-03 12:37:51 -0500

    Setup for Design Patterns Practice

---
+++ interviewPrepWithObjC/HTTPClient.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface HTTPClient : NSObject
+
+- (id)getRequest:(NSString*)url;
+- (id)postRequest:(NSString*)url body:(NSString*)body;
+- (UIImage*)downloadImage:(NSString*)url;
+
+@end
